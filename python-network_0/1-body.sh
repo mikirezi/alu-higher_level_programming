c5a7005741189e6e8180aa562bebe276be281e29
mikirezi <your-email@example.com>
2026-08-01 18:59:42 +0000

    Fix 1-body.sh to follow redirects with -L flag

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays body of response only if status code is 200
-response=$(curl -s -w "%{http_code}" "$1"); http_code=${response: -3}; body=${response%???}; [ "$http_code" = "200" ] && echo -n "$body"
+response=$(curl -s -L -w "%{http_code}" "$1"); http_code=${response: -3}; body=${response%???}; [ "$http_code" = "200" ] && echo -n "$body"
